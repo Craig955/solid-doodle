eb901aa440a553f783df68ad1f1acf11b3f0fac2
Craig955 <62149749+Craig955@users.noreply.github.com>
2020-11-02 16:43:53 +0000

    Add files via upload

---
+++ Mojave_Erase_And_Install.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+#Fresh installation of Mojave
+###ERASES APFS VOLUME###
+
+sudo pmset -a sleep 0 disksleep 0 displaysleep 0
+sleep 1
+sudo /Applications/Install\ macOS\ Mojave.app/Contents/Resources/startosinstall --agreetolicense --eraseinstall --newvolumename "Macintosh HD" --nointeraction
+
+exit 0
