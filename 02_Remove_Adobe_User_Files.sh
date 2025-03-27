554f272db93c3acaeb98e6aca35301579affd3b9
Craig955 <62149749+Craig955@users.noreply.github.com>
2025-03-27 11:16:57 +0000

    Delete 02_Remove_Adobe_User_Files.sh

--- 02_Remove_Adobe_User_Files.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rm -rf ~/Library/Application\ Support/Adobe* ~/Library/Application\ Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.adobe* ~/Library/Application\ Support/CrashReporter/Adobe* ~/Library/Caches/Adobe ~/Library/Caches/com.Adobe.* ~/Library/Caches/com.adobe.* ~/Library/Cookies/com.adobe.* ~/Library/Logs/Adobe* ~/Library/PhotoshopCrashes ~/Library/Preferences/Adobe* ~/Library/Preferences/com.adobe.* ~/Library/Preferences/Macromedia* ~/Library/Saved\ Application\ State/com.adobe.*
